67e5f753490e431827732cf3e2fb453ec3562530
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-25 23:39:59 -0400

    creating a MySQL database

---
+++ VS_Flask_dev/FlaskWebProject/Database_creation.sql
@@ -0,0 +1,4 @@
+create table users(id int(11) auto_increment primary key, 
+name varchar(100), email varchar(100),
+username varchar(30), password varchar(100), 
+register_date timestamp default current_timestamp);
