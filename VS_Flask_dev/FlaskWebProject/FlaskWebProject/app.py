d9ced60308635c460fdd9c9504f3320f6a23adf3
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-27 15:04:23 -0400

    connecting to the database

--- VS_Flask_dev/FlaskWebProject/FlaskWebProject/app.py
+++ VS_Flask_dev/FlaskWebProject/FlaskWebProject/app.py
@@ -8,6 +8,20 @@ from passlib.hash import sha256_crypt
 ## Instance of flask class
 app = Flask(__name__)
 
+## Congigure MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'python_flask'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+## Initilize MySQL
+mysql = MySQL(app)
+
+
+
+
+
 Articles = Articles()
 #debug=True ## here or below. need to remoce when production
 
@@ -34,7 +48,7 @@ def article(id):
     
 
 ## Defining WTForm class
-class RegisterForm(Form):
+class RegisterForm(Form): ## object is request.form type
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
@@ -45,8 +59,31 @@ class RegisterForm(Form):
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
+    form = RegisterForm(request.form)  ## creating an object instance from RegisterForm class
     if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        ## Create a curser
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", 
+                    (name, email, username, password))
+
+        ## Comit to db
+        mysql.connection.commit()
+
+        ## Close connection
+        cur.close()
+
+        flash('Your are now registered and can log in', 'Success')
+        redirect(url_for('index'))
+
+
+
+
         return render_template('register.html')
     return render_template('register.html', form=form)
 
