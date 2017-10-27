b18287e1d435785ef91292c966c614289525a645
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-26 23:28:24 -0400

    Registration form app.py

--- VS_Flask_dev/FlaskWebProject/FlaskWebProject/app.py
+++ VS_Flask_dev/FlaskWebProject/FlaskWebProject/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, flash, url_for, logging, session
+from flask import Flask, render_template, flash, url_for, logging, session, request
 from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, PasswordField, validators
@@ -28,7 +28,7 @@ def about():
 def articles():
     return render_template('articles.html', articles = Articles)
 
-@app.route("/article/<string:id>/")
+@app.route('/article/<string:id>/')
 def article(id):
    return render_template('article.html', id=id)
     
@@ -40,14 +40,15 @@ class RegisterForm(Form):
     email = StringField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', 'message'='Password do not match')])
+        validators.EqualTo('confirm', message='Password do not match')])
     confirm = PasswordField('Confirm Password')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html', form=form)
+        return render_template('register.html')
+    return render_template('register.html', form=form)
 
 
 
